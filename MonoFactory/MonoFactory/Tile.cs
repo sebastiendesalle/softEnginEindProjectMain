c981eaadc5327fe0071251deb27deba6f5629054
sebastien <sebastien.desalle@student.ap.be>
2025-11-30 02:06:53 +0100

    Add file

---
+++ MonoFactory/MonoFactory/Tile.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoFactory
+{
+    public class Tile // flyweight pattern
+    {
+        public bool IsSolid { get; set; }
+
+        public Texture2D Texture { get; set; }
+
+        public Tile(Texture2D texture, bool isSolid)
+        {
+            Texture = texture;
+            IsSolid = isSolid;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            spriteBatch.Draw(Texture, position, Color.White);
+        }
+    }
+}
