643c97e0549023b7eb199c3f133d882f6e464b4a
sebastien <sebastien.desalle@student.ap.be>
2025-12-06 23:44:30 +0100

    add chest script

---
+++ MonoFactory/MonoFactory/Chest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
+
+namespace MonoFactory
+{
+    public class Chest: IGameObject, IInteractable
+    {
+        public Vector2 Position { get; private set; }
+        private Texture2D _texture;
+        private bool _isOpen = false;
+
+        public Chest(Texture2D texture, Vector2 position)
+        {
+            _texture = texture;
+            Position = position;
+        }
+
+        public void Interact(Hero hero)
+        {
+            _isOpen = !_isOpen;
+            Debug.WriteLine($"Chest is {(_isOpen ? "open" : "closed")}");
+        }
+
+        public void Update(GameTime gameTime) { }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Color c = _isOpen ? Color.Gray : Color.White;
+
+            spriteBatch.Draw(_texture, Position, c);
+        }
+    }
+}
