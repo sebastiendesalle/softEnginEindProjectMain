2dc07faafb99a6c4ea6e971833369e9c17f03cd7
sebastien <sebastien.desalle@student.ap.be>
2025-12-10 16:55:35 +0100

    Modify for jump mechanics

--- MonoFactory/MonoFactory/PhysicsComponent.cs
+++ MonoFactory/MonoFactory/PhysicsComponent.cs
@@ -8,10 +8,16 @@ using System.Threading.Tasks;
 
 namespace MonoFactory
 {
-    class PhysicsComponent // handles movement and gravity and collisions
+    public class PhysicsComponent // handles movement and gravity and collisions
     {
         public Vector2 Position;
         public Vector2 Velocity;
+
+        public float Height { get; private set; } = 0f;
+        public float VerticalVelocity { get; private set; } = 0f;
+
+        private const float Gravity = 2000f; // pulls down
+        private const float JumpForce = -800f; // todo: test if needs invertion
 
         private float friction = 0.85f; // make movement feel good
         private float speed = 50f; // acceleration speed
@@ -31,6 +37,14 @@ namespace MonoFactory
             }
         }
 
+        public void Jump()
+        {
+            if (Height <= 0) // only jump if on ground
+            {
+                VerticalVelocity = 600;
+            }
+        }
+
         public void Update(float deltaTime)
         {
             // set velocity
@@ -42,6 +56,18 @@ namespace MonoFactory
             if (Velocity.Length() < 0.1f)
             {
                 Velocity = Vector2.Zero;
+            }
+
+            if (Height > 0 || VerticalVelocity != 0)
+            {
+                Height += VerticalVelocity * deltaTime;
+                VerticalVelocity -= Gravity * deltaTime;
+            }
+
+            if (Height <= 0)
+            {
+                Height = 0;
+                VerticalVelocity = 0;
             }
         }
     }
