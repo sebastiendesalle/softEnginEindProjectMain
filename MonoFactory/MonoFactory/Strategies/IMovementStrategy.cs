7561d040b6dcd10430f1044d2b69605a2cca0f52
sebastien <sebastien.desalle@student.ap.be>
2025-12-10 19:32:40 +0100

    Add movement interface

---
+++ MonoFactory/MonoFactory/Strategies/IMovementStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoFactory.Strategies
+{
+    public interface IMovementStrategy
+    {
+        Vector2 Move(GameTime gameTime, Vector2 currentPosition, Vector2 targetPosition);
+    }
+}
