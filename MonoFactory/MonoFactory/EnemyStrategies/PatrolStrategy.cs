fe58c51b2d47443df3e4c8e6d888c92ac3505855
sebastien <sebastien.desalle@student.ap.be>
2025-12-10 19:39:17 +0100

    Add patrol strategy file

---
+++ MonoFactory/MonoFactory/EnemyStrategies/PatrolStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoFactory.EnemyStrategies
+{
+    public class PatrolStrategy : IMovementStrategy
+    {
+        public Vector2 Move(GameTime gameTime, Vector2 currentPosition, Vector2 targetPosition)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
