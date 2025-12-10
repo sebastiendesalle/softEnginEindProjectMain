616a13fa718e2a2112b6fead4aaaba94c0b2d8fc
sebastien <sebastien.desalle@student.ap.be>
2025-12-10 18:23:02 +0100

    Add EntityFactory

---
+++ MonoFactory/MonoFactory/EntityFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace MonoFactory
+{
+    public class EntityFactory
+    {
+        // store textures
+        private Dictionary<string, Texture2D> _textureLibrary;
+
+        public EntityFactory()
+        {
+            _textureLibrary = new Dictionary<string, Texture2D>();
+        }
+
+        public void RegisterTexture(string key, Texture2D texture)
+        {
+            if (!_textureLibrary.ContainsKey(key))
+            {
+                _textureLibrary.Add(key, texture);
+            }
+        }
+
+        public IGameObject CreateEntity(string type, Vector2 position)
+        {
+            if (!_textureLibrary.ContainsKey(type))
+            {
+                throw new Exception($"Texture for {type} not found.");
+            }
+
+            Texture2D texture = _textureLibrary[type];
+
+            switch (type)
+            {
+                case "Goblin":
+                    return new Enemy(texture, position);
+                case "Chest":
+                    return new Chest(texture, position);
+                // TODO: add machine
+                default:
+                    throw new Exception($"Entity type {type} is not in factory switch");
+            }
+        }
+    }
+}
