71c26202fb66e2aba425ef69ebf1a97217eecb89
sebastien <sebastien.desalle@student.ap.be>
2025-12-10 18:15:32 +0100

    Add enemy object class

---
+++ MonoFactory/MonoFactory/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoFactory
+{
+    public class Enemy: IGameObject
+    {
+        private Texture2D _texture;
+        public Vector2 Position;
+
+        public Enemy(Texture2D texture, Vector2 startPosition)
+        {
+            _texture = texture;
+            Position = startPosition;
+        }
+
+        public Rectangle Rectangle => new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
+
+        public void Update(GameTime gameTime)
+        {
+            // TODO: add ai
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Position, Color.White);
+        }
+    }
+}
