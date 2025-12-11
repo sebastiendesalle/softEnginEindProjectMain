90e3c2c8705d8c48bf87a2b6a0a73d861e4399be
sebastien <sebastien.desalle@student.ap.be>
2025-12-11 09:47:13 +0100

    Add patrol strat

--- MonoFactory/MonoFactory/EnemyStrategies/PatrolStrategy.cs
+++ MonoFactory/MonoFactory/EnemyStrategies/PatrolStrategy.cs
@@ -4,9 +4,32 @@ namespace MonoFactory.EnemyStrategies
 {
     public class PatrolStrategy : IMovementStrategy
     {
+        private float _speed = 60f;
+        private Vector2 _startPoint;
+        private Vector2 _endPoint;
+        private bool _movingToEnd = true;
+
+        public PatrolStrategy(Vector2 start, Vector2 end)
+        {
+            _startPoint = start;
+            _endPoint = end;
+        }
         public Vector2 Move(GameTime gameTime, Vector2 currentPosition, Vector2 targetPosition)
         {
-            throw new System.NotImplementedException();
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Vector2 target = _movingToEnd ? _endPoint : _startPoint;
+
+            Vector2 direction = target - currentPosition;
+            float distance = direction.Length();
+
+            if (distance < 5f)
+            {
+                _movingToEnd = !_movingToEnd;
+                return currentPosition;
+            }
+
+            direction.Normalize();
+            return currentPosition + (direction * _speed * dt);
         }
     }
 }
