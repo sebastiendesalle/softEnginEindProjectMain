1677aee37157d95bc459da74a63c790f2f1940d9
sebastien <sebastien.desalle@student.ap.be>
2025-12-11 09:48:17 +0100

    Add stationary strat

---
+++ MonoFactory/MonoFactory/EnemyStrategies/StationaryStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoFactory.EnemyStrategies
+{
+    public class StationaryStrategy : IMovementStrategy
+    {
+        public Vector2 Move(GameTime gameTime, Vector2 currentPosition, Vector2 targetPosition)
+        {
+            return currentPosition;
+        }
+    }
+}
