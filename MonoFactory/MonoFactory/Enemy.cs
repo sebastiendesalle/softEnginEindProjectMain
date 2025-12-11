5c44156ee55cf4933069206a08a9567b1dba0547
sebastien <sebastien.desalle@student.ap.be>
2025-12-11 09:54:38 +0100

    Update enemy script for enemy ai

--- MonoFactory/MonoFactory/Enemy.cs
+++ MonoFactory/MonoFactory/Enemy.cs
@@ -1,29 +1,49 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoFactory.EnemyStrategies;
 
 namespace MonoFactory
 {
-    public class Enemy: IGameObject
+    public class Enemy: IGameObject, IInteractable
     {
         private Texture2D _texture;
-        public Vector2 Position;
+        public Vector2 Position { get; private set; }
 
-        public Enemy(Texture2D texture, Vector2 startPosition)
+        private IMovementStrategy _movementStrategy;
+        private Hero _targetHero;
+        private Rectangle _sourceRect = new Rectangle(0, 0, 64, 64);
+
+        public Enemy(Texture2D texture, Vector2 startPosition, IMovementStrategy strategy)
         {
             _texture = texture;
             Position = startPosition;
+            _movementStrategy = strategy;
+        }
+
+        public void SetTarget(Hero hero)
+        {
+            _targetHero = hero;
         }
 
         public Rectangle Rectangle => new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
 
         public void Update(GameTime gameTime)
         {
-            // TODO: add ai
+            if (_movementStrategy != null)
+            {
+                Vector2 targetPos = _targetHero != null ? _targetHero.Position : Position;
+                Position = _movementStrategy.Move(gameTime, Position, targetPos);
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_texture, Position, Color.White);
+            spriteBatch.Draw(_texture, Position,_sourceRect, Color.White);
+        }
+
+        public void Interact(Hero hero)
+        {
+            // TODO: implement logic for taking damage from enemy
         }
     }
 }
