bb10c78e5c71af9613274ed57c031b3d2cd04b88
sebastien <sebastien.desalle@student.ap.be>
2026-01-09 18:46:59 +0100

    Add recipe class for crafting

---
+++ MonoFactory/MonoFactory/Items/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoFactory.Items
+{
+    public class Recipe
+    {
+        public List<String> Ingredients { get; private set; }
+
+        public IItem Result { get; private set; }
+
+        public Recipe(List<string> ingredients, IItem result)
+        {
+            Ingredients = ingredients;
+            Result = result;
+        }
+
+        public bool Matches(List<string> inputItemIds)
+        {
+            if (inputItemIds.Count != Ingredients.Count)
+            {
+                return false;
+            }
+
+            var sortedIngredients = Ingredients.OrderBy(x => x).ToList();
+            var sortedInput = inputItemIds.OrderBy(x => x).ToList();
+
+            return sortedIngredients.SequenceEqual(sortedInput);
+        }
+    }
+}
